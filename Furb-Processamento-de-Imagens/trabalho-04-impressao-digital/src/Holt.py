65fc117571e88825c4862fab6cc9edc54d55584c
Djonathan <odjonathankrause@gmail.com>
2018-10-16 18:42:09 -0300

    holt

---
+++ Furb-Processamento-de-Imagens/trabalho-04-impressao-digital/src/Holt.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+__author__ = "Djonathan Krause"
+__credits__ = "Baseado na implementação Java https://github.com/marinalaux/PDI"
+__email__ = "odjonathankrause@gmail.com"
+__license__ = "MIT"
+
+class Holt:
+    def apply(self, img):
+        result = img.copy()
+        process = img.copy()
+        odd = False
+    
+        # linha/y/h, colunas/x/w
+        height, width = process.shape
+        for x in range(0, width-1):
+            for y in range(0, height-1):
+                if self.v(process[y][x]):
+                    neighborhood = self.getNeighborhood(x, y, process)
+                    value = self.pixelValue(neighborhood, odd)
+                    result[y][x] = value
+            process[y][x] = result[y][x]
+        return result
+    
+    def getNeighborhood(self, x, y, img):
+        neighbor = np.zeros((3,3))
+        for i in range(1, 3):
+            for j in range(1, 3):
+                x2 = x + i - 1
+                y2 = y + j - 1
+                neighbor[i][j] = img[y2][x2]
+        return neighbor
+    
+    def pixelValue(self, pixels, odd):
+        neighborhood = self.neighborhoodToArray(pixels)
+        if not self.isEdge(neighborhood):
+            return pixels[1][1]
+    
+        n = pixels[1][0]
+        s = pixels[1][2]
+        l = pixels[2][1]
+        o = pixels[0][1]
+    
+        if odd:
+            if self.v(l) and self.v(s) and (self.v(n) or self.v(o)):
+                return pixels[1][1]
+        else:
+            if self.v(o) and self.v(n) and (self.v(s) or self.v(l)):
+                return pixels[1][1]
+        return 0
+    
+    def isEdge(self, n):
+        nConn = n[0] + n[1] + n[2] + n[3] + n[4] + n[5] + n[6] + n[7]
+        return (nConn >= 2 and nConn <= 6) and self.isConnected(n)
+    
+    def isConnected(self, n):
+        connection = (1 if n[0] < n[1] else 0) + (1 if n[1] < n[2] else 0) + (1 if n[2] < n[3] else 0) + (1 if n[3] < n[4] else 0) + (1 if n[4] < n[5] else 0) + (1 if n[5] < n[6] else 0) + (1 if n[6] < n[7] else 0) + (1 if n[7] < n[0] else 0)
+        return connection == 1
+    
+    def neighborhoodToArray(self, pixels):
+        p2 = pixels[1][0] / 255
+        p3 = pixels[2][0] / 255
+        p4 = pixels[2][1] / 255
+        p5 = pixels[2][2] / 255
+        p6 = pixels[1][2] / 255
+        p7 = pixels[0][2] / 255
+        p8 = pixels[0][1] / 255
+        p9 = pixels[0][0] / 255
+    
+        return [p2, p3, p4, p5, p6, p7, p8, p9]
+    
+    def v(self, pixel):
+        return pixel == 255
--- Furb-Processamento-de-Imagens/trabalho-04-impressao-digital/src/__init__.py
+++ Furb-Processamento-de-Imagens/trabalho-04-impressao-digital/src/__init__.py
@@ -1,10 +1,12 @@
 import cv2
 import numpy as np
+from Holt import Holt
+#from CrossingNumber import CrossingNumber
 
 ###################
 
 # 1. Aquisição da imagem. 0 = cinza
-img = cv2.imread("../dataset/1_1.png", 0)
+img = cv2.imread("../dataset/img_artigo.jpg", 0)
 
 # 2. Filtro da mediana
 mediana = cv2.medianBlur(img, 5)
@@ -21,19 +23,21 @@ sharp = cv2.filter2D(mediana, -1, filtro)
 ret, otsu = cv2.threshold(sharp, 0, 255, cv2.THRESH_OTSU)
 
 # 5. Afinamento método de Holt
-#holt_1 = np.array([[0, 1, x], [1, 1, x], [x, x, 0]])
-#holt_2 = np.array([[x, 1, 0], [x, 1, 1], [0, x, x]])
-#holt_3 = np.array([[0, x, x], [x, 1, 1], [x, 1, 0]])
-#holt_4 = np.array([[x, x, 0], [1, 1, x], [0, 1, x]])
+holt = Holt()
+holt_resultado = holt.apply(otsu)
+holt_resultado= cv2.bitwise_not(holt_resultado)
 
-zhang_suen = thinning(otsu)
+# 6. Crossing Number. Detectar cristas finais e bifurcações
+# 6.1 Detectar cristas finais
+
 
 # Amostra as ibagens
 cv2.imshow('img', img)
 cv2.imshow('mediana', mediana)
 cv2.imshow('sharp', sharp)
 cv2.imshow('filtro otsu', otsu)
-cv2.imshow('zhang suen', zhang_suen)
+cv2.imshow('holt', holt_resultado)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
